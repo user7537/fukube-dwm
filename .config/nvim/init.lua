c3ec722113585b6bc0f878216b4beccbeb542125
user7537 <renn233580@gmail.com>
2025-10-25 20:33:28 +0530

    Changed my entire nvim config multiple times, probably 40 times in the last month or two, tried out nightly and native package managing  and finally decided to just use lazy. Idk just thought using that is much more efficient cause its stable and just tested and I use like 10 plugins at most so it doesn't really help the performance that much either. So here you go.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,183 @@
-require("core.options")
-require("core.keymaps")
-require("core.lazy") -- plugin manager and plugin loading
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" }, { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+--OPTIONS
+vim.cmd([[set mouse=]])
+vim.opt.winborder = "rounded"
+vim.opt.hlsearch = false
+vim.opt.tabstop = 2
+vim.opt.cursorcolumn = false
+vim.opt.ignorecase = true
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.number = true
+vim.opt.mouse= 'a'
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+vim.opt.undofile = true
+vim.opt.signcolumn = "yes"
+vim.opt.ruler = false
+vim.opt.clipboard = "unnamedplus"
+
+--MAPPINGS
+local map = vim.keymap.set
+vim.g.mapleader = " "
+map('n', '<leader>o', ':update<CR> :source<CR>')
+map('n', '<leader>w', ':write<CR>')
+map('n', '<leader>q', ':quit<CR>')
+map('n', '<leader>s', ':e #<CR>')
+map('n', '<leader>S', ':sf #<CR>')
+map({ 'n', 'v' }, '<leader>y', '"+y')
+map({ 'n', 'v' }, '<leader>d', '"+d')
+map({ 'n', 'v' }, '<leader>c', '1z=')
+map('n', '<leader>e', ':NvimTreeToggle<CR>')
+
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+      {"tahayvr/matteblack.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme "matteblack"
+  end,
+	},
+  { "nvim-tree/nvim-web-devicons", lazy = true },
+
+	{"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"},
+
+  { "neovim/nvim-lspconfig"  },
+  
+	{'nvim-lua/plenary.nvim'  },
+
+	{"windwp/nvim-autopairs"},
+  
+	{ "nvimtools/none-ls.nvim", event = "VeryLazy", 
+	opts = function()
+	local null_ls = require("null-ls")
+
+	null_ls.setup({
+  	sources = {
+        null_ls.builtins.formatting.clang_format,
+    },
+		})
+	end,
+	},
+
+	{	"mason-org/mason.nvim", opts = {
+		ensure_installed = {
+				"prettier",
+				"eslint_d",
+				"clangd",
+				"clang-format",
+			},
+		}, lazy = true },
+	
+	{
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",},
+	},
+
+	{  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup {}
+  end,
+	},
+	{
+  'chomosuke/typst-preview.nvim',
+  lazy = false, -- or ft = 'typst'
+  version = '1.*',
+  opts = {}, -- lazy.nvim will implicitly calls `setup {}`
+	},
+
+	{  "mfussenegger/nvim-lint",
+  event = { "BufReadPost", "BufWritePost" },
+  config = function()
+    local lint = require("lint")
+
+    lint.linters_by_ft = {
+      javascript = { "eslint_d" },
+      typescript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+      svelte = { "eslint_d" },
+      c = { "cpplint" },
+      cpp = { "cpplint" },
+    }
+
+    local group = vim.api.nvim_create_augroup("lint", { clear = true })
+    vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost" }, {
+      group = group,
+      callback = function()
+        lint.try_lint()
+      end,
+    })
+
+    vim.keymap.set("n", "<leader>ll", function()
+      lint.try_lint()
+    end, { desc = "Trigger linting for current file" })
+
+    vim.diagnostic.config({
+      virtual_text = true,
+      signs = true,
+      underline = true,
+    })
+  end,
+}
+	},
+
+  -- Configure any other settings here. See the documentation for more details.
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
+
+
+vim.lsp.enable({
+	"svelte", "tinymist",
+	"clangd", 
+	"typescript-language-server" 
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = vim.api.nvim_create_augroup('my.lsp', {}),
+	callback = function(args)
+		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+		if client:supports_method('textDocument/completion') then
+			-- Optional: trigger autocompletion on EVERY keypress. May be slow!
+			local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+			client.server_capabilities.completionProvider.triggerCharacters = chars
+			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+		end
+	end,
+})
+vim.cmd [[set completeopt+=menuone,noselect,popup]]
+vim.cmd("hi statusline guibg=NONE")
+
+
 
